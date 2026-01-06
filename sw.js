aeb0ddb4dd2016e36ab78c39915cd33da7ac2130
bigviv <73597575+bigviv@users.noreply.github.com>
2026-01-06 17:17:52 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'zerolog-v1';
+const CACHE_NAME = 'zerolog-v2';
 const ASSETS = [
   '/zerolog/index.html',
   '/zerolog/manifest.json',
@@ -6,14 +6,16 @@ const ASSETS = [
   '/zerolog/screenshot.png'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
